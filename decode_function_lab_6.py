7149b80cc140fd6c1b8b023ee1b4d82eae9a7bc8
Bernardo <bernardojavier18@gmail.com>
2023-03-08 23:06:26 -0500

    Cast user input to int. Implemented encode function. Fixed bug in decode function.

--- decode_function_lab_6.py
+++ decode_function_lab_6.py
@@ -8,18 +8,23 @@ def main():
              f'3. Quit\n')
         encoded_password = ''
         password = ''
-        user_input = input('Please enter an option: ')
+        user_input = int(input('Please enter an option: '))
         if user_input == 1:
             password = input('Please enter your password to encode: ')
             encoded_password = encode(password)
             print('Your password has been encoded and stored!')
         elif user_input == 2:
+            encoded_password = input('Please enter your password to decode: ')
             password = decode(encoded_password)
             print(f'The encoded password is {encoded_password}, and the original password is {password}')
 
 
 def encode(password):
-   pass
+       #Takes in a string password (int only) and shifts each integer up by 3
+    encoded = []
+    for char in password:
+        encoded.append(str((int(char)+3)%10))
+    return ''.join(encoded)
 
 
 def decode(encoded_password):
@@ -27,10 +32,10 @@ def decode(encoded_password):
     decoded_password = ""
     for digit in encoded_password:
         # Cast digit to an int and add 3. % 10 to get the first digit after adding.
-        digit = (int(digit) + 3) % 10
+        digit = (int(digit) - 3) % 10
         # Add each digit to a new string
-        encoded_password += str(digit)
-    return encoded_password
+        decoded_password += str(digit)
+    return decoded_password
 
 
 if __name__ == '__main__':
